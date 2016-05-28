8f66c1da0f7d8ec1907a0658d5e1d599a1914dd0
Jack Higgins <jackhiggins07@gmail.com>
2016-05-28 14:05:25 +0100

    Adding global layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,40 @@
 module ApplicationHelper
+  # Bootstrap style alerts - http://getbootstrap.com/components/#alerts
+  def alert(alert_level)
+    content_tag :div, class: ['alert', "alert-#{alert_level}", 'alert-dismissible'], role: 'alert' do
+      button = content_tag :button, type: 'button', class: 'close', 'data-dismiss': 'alert', 'aria-label': 'Close' do
+        content_tag :span, sanitize('&times;'), 'aria-hidden': 'true'
+      end
+
+      concat content_tag :span, '', class: 'glyphicon glyphicon-exclamation-sign', 'aria-hidden': true
+      concat button
+      yield
+    end
+  end
+
+  # Bootstrap navbar
+  def nav_bar
+    content_tag :nav, class: "nav navbar-nav" do
+      yield
+    end
+  end
+
+  # Extension of 'link_to' for bootstrap
+  # Sets "class='active'" in the <li> tag"
+  def nav_link(text, path)
+    if current_page? path
+      content_tag :li, class: 'active'  do
+
+        # <span class="sr-only">(current)</span> required for Screen Readers
+        link_to path do
+          concat text
+          concat content_tag :span, '(current)', class: 'sr-only'
+        end
+      end
+    else
+      content_tag :li do
+        link_to text, path
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'about/index'
+
   get 'home/index'
 
   root 'home#index'
